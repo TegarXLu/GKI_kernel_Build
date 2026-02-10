25efdf1c4aae337fcfe433271f80ff7a9e085e7c
TegarXLu <botdimas67@gmail.com>
2026-02-10 22:57:42 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -205,9 +205,9 @@ fi
 if ksu_manual_hook; then
   log "Applying manual hook patch"
   if [[ "$KSU" == "Suki" ]]; then
-    patch -p1 --forward < $WORKDIR/kernel-patches/manual-hook-v1.6.patch
+    patch -p1 --forward < $WORKDIR/kernel-patches/hooks/manual-hook-v1.6.patch
   else
-    patch -p1 --forward < $WORKDIR/kernel-patches/manual-hook-v1.4.patch
+    patch -p1 --forward < $WORKDIR/kernel-patches/hooks/manual-hook-v1.4.patch
   fi
   config --enable CONFIG_KSU_MANUAL_HOOK
   config --disable CONFIG_KSU_KPROBES_HOOK
